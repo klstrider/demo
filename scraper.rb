bb5506a7ecb7cb18432dc173369c52cbc1567888
shyamperi <shyam.peri@acm.org>
2016-10-21 18:52:19 +0530

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,3 @@
 #<Encoding:utf-8>
-require_relative 'mechanize.rb'
-require 'awesome_print'
 require 'metainspector'
 puts MetaInspector.new('http://khoslalabs.com/', :allow_redirections => false).to_hash.to_json
